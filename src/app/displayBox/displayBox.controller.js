503cd2bd11cd6a5c22fb59d6704c37c5302acf79
k_zenchyk <k_zenchyk@wargaming.net>
2016-02-27 16:50:26 +0300

    + add UI

---
+++ src/app/displayBox/displayBox.controller.js
@@ -0,0 +1,9 @@
+/**
+ * Created by k_zenchyk on 2/26/16.
+ */
+import displayService from './displayBox.service.js';
+
+export default function displayController($scope) {
+    var vm = this;
+    vm.hi = "I am a Display Box";
+}
