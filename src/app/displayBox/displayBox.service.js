02d04d9e0eda956e8393c69c746443e7f4f09df1
k_zenchyk <k_zenchyk@wargaming.net>
2016-02-29 23:57:37 +0300

    * display messages from API with interval

--- src/app/displayBox/displayBox.service.js
+++ src/app/displayBox/displayBox.service.js
@@ -2,6 +2,10 @@
  * Created by k_zenchyk on 2/26/16.
  */
 
-export default function displayService() {
+export default function displayService($http) {
+    var baseUrl = 'http://localhost:3000/db';
 
+    this.getMessages = function(){
+        return $http.get(baseUrl)
+    }
 }
